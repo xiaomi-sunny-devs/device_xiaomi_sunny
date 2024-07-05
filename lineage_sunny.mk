0d78cfe38bb49e6cbf69278513e21005ab156732
eklerismunir <munir.nasibzade@proton.me>
2024-07-05 22:54:41 +0400

    sunny: YAAP -> AlphaDroid - adapt AndroidProducts.mk - remove no longer needed rros - add overlay-lineage - yaap_sunny.mk -> lineage_sunny.mk and adapt - remove yaap.dependencies - adapt sensors and disableqti package - added Alphadroid flags
    Signed-off-by: eklerismunir <munir.nasibzade@proton.me>

--- lineage_sunny.mk
+++ lineage_sunny.mk
@@ -11,12 +11,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sunny device
 $(call inherit-product, device/xiaomi/sunny/device.mk)
 
-# Inherit some common YAAP stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+# Inherit some common AlphaDroid stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device config
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := eklerismunir
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := yaap_sunny
+PRODUCT_NAME := lineage_sunny
 PRODUCT_DEVICE := sunny
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := M2101K7AG
@@ -27,3 +40,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := Redmi/sunny_global/sunny:12/RKQ1.210614.002/V14.0.8.0.SKGMIXM:user/release-keys
